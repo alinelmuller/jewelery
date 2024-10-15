a2d70df91c359f4e9930e5cd59e9e0eaeca94f8e
jesstheleftie <jessetang57@hotmail.com>
2024-10-15 16:21:30 -0700

    crud brand and jewelery

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -9,6 +9,64 @@ const getAllBrands = async (req, res) => {
     }
   };
 
+  const getBrandByName = async (req, res) => {
+    try {
+      const { brandName } = req.params;
+      const brand = await Brand.findOne({ name: brandName });
+  
+      if (brand) {
+        res.status(200).json(brand);
+      } else {
+        res.status(404).json({ message: "Brand not found" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: "Server Error" });
+    }
+  };
+
+  const createBrand = async (req, res) => {
+    try {
+      const brand = new Brand(req.body);
+      console.log("req.body", req.body, "brand", brand);
+     let saved = await brand.save();
+     console.log('saved',saved)
+      return res.status(201).json({ brand });
+    } catch (error) {
+      return res.json({ error: error.message });
+    }
+  };
+
+  const updateBrand = async (req, res)=>{
+    try{
+let {id} = req.params
+let brand= await Brand.findByIdAndUpdate(id, req.body, {new:true})
+if(brand){
+    return res.status(200).json(brand)
+}
+throw new Error("Brand not found")
+    }catch (error){
+        return res.status(500).send(error.message)
+    }
+}
+
+const deleteBrand = async (req,res)=>{
+    try{
+const {id} = req.params
+const deleted = await Brand.findByIdAndDelete(id)
+if (deleted){
+    return res.status(200).send("Brand deleted")
+}
+throw new Error("Brand not found")
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
+}
+
+
   module.exports ={
-    getAllBrands
+    getAllBrands,
+    getBrandByName,
+    createBrand,
+    updateBrand,
+    deleteBrand
   }
--- server.js
+++ server.js
@@ -7,10 +7,23 @@ const jeweleryController = require ('./controllers/jewleryController')
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 //CRUD for Brands
+app.put('/brands/:id', brandController.updateBrand)
+app.delete('/brands/:id', brandController.deleteBrand)
+app.get('/brands/:brandName', brandController.getBrandByName)
 app.get('/brands', brandController.getAllBrands)
+app.post('/brands', brandController.createBrand)
 
 //CRUD for Jewleries
+app.put('/jeweleries/:id', jeweleryController.updateJewelery)
+app.delete('/jeweleries/:id', jeweleryController.deleteJewelery)
+app.get('/jeweleries/:jeweleryName', jeweleryController.getJeweleryByName)
 app.get('/jeweleries', jeweleryController.getAllJeweleries)
+app.post('/jeweleries', jeweleryController.createJewelery)
