b6d498f11642ff1336d0fb35f2026214efc72e8f
pedrocruz <JayJMarquez05@gmail.com>
2024-10-15 17:18:56 -0500

    Template I think? This is what we will have for the product

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const db = require("./db");
-//const plantController = require('./controllers/plantController')
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
