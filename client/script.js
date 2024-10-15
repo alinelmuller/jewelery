e72027e296a8356ba4a944b82eb0de82623ac22c
pedrocruz <JayJMarquez05@gmail.com>
2024-10-15 18:57:45 -0500

    Nothing updated. Files did not save?

--- client/script.js
+++ client/script.js
@@ -1,9 +1,9 @@
-/* ------------{Global Variables}---------------- */
-const Brand = require(`../models/brand`);
-const Jewelery = require(`../models/jewelery`);
-const 
-/* ------------{Functions}---------------- */
-async function fetchJeweleries(Object) {
-  const res = await axios.get(`http://localhost:3002/jeweleries`);
-}
-/* ------------{Event Listeners}---------------- */
+// /* ------------{Global Variables}---------------- */
+// const Brand = require(`../models/brand`);
+// const Jewelery = require(`../models/jewelery`);
+// const
+// /* ------------{Functions}---------------- */
+// async function fetchJeweleries(Object) {
+//   const res = await axios.get(`http://localhost:3002/jeweleries`);
+// }
+// /* ------------{Event Listeners}---------------- */
