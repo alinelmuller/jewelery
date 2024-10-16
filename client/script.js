eb07cf3073fb561d32a674abf1e5614704edeade
pedrocruz <JayJMarquez05@gmail.com>
2024-10-16 09:03:18 -0500

    Got axios finally working. Fetched data for product: Name | Image | Price | Description

--- client/script.js
+++ client/script.js
@@ -1,13 +1,53 @@
-/* ------------{Global Variables}---------------- */
-const Brand = require(`../models/brand`);
-const Jewelery = require(`../models/jewelery`);
-const button = document.querySelector(`#submit`);
-const priceOfProduct = document.querySelector(`#price-of-product`);
-const productMenuDrop = document.querySelector(`#see-more-button`);
+// /*-----------------{Global Variables}-----------------*/
+const categoryTitle = document.querySelector(`#catergory-title`);
+
+const productImage = document.querySelector(`#product-image`);
+const brandImage = document.querySelector(`#brand-image`);
+
+const productPriceTag = document.querySelector(`#price-of-product`);
 const productName = document.querySelector(`#product-name`);
 const productDescription = document.querySelector(`#product-description`);
-/* ------------{Functions}---------------- */
-async function userSearchForProduct(name) {
-  const res = await axios.get(`http://localhost:3002/jeweleries/${name}`);
+
+// const brandOfProduct = document.querySelector(`#product-brand`);
+// const originOfBrand = document.querySelector(`#brand-origin`);
+// /*--------------------{Functions}---------------------*/
+async function fetchAllJewelry() {
+  try {
+    const res = await axios.get(`http://localhost:3002/jeweleries`);
+    const apiLibrary = res.data;
+    console.log(apiLibrary);
+
+    apiLibrary.forEach((api) => {
+      categoryTitle.textContent = api.category;
+
+      productImage.src = api.image;
+      productImage.alt = `${api.name} image`;
+
+      productName.textContent = api.name;
+      productPriceTag.textContent = `$${api.price}`;
+      productDescription.textContent = api.description;
+
+      // async function fetchAllBrands() {
+      //   try {
+      //     const brandRes = await axios.get(
+      //       `http://localhost:3002/brands/${api.brand}`
+      //     );
+      //     const brandData = brandRes.data;
+
+      //     brandOfProduct.textContent = brandData.name;
+      //     originOfBrand.textContent = brandData.origin;
+      //   } catch (brandE) {
+      //     console.error(`Error fetching brand:`, brandE);
+      //     // The URL is not built MUST remember to add it in later!
+      //     brandOfProduct.textContent = `404 Error | Please search again at our <a href="Place holder for our URL">home page.</a>`;
+      //   }
+      // }
+      // fetchAllBrands();
+    });
+  } catch (e) {
+    console.error("Error fetching jewelry:", e);
+  }
 }
-/* ------------{Event Listeners}---------------- */
+
+// /*--------------{Running Function/Logs}---------------*/
+fetchAllJewelry();
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const db = require("./db");
 const brandController = require("./controllers/brandController");
-const jeweleryController = require("./controllers/jewleryController");
+const jewelryController = require("./controllers/jewelryController");
+
+// Need to add cors for script.js to work - Pedro
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+
+// the app call for cors - Pedro
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
@@ -17,7 +23,7 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 app.get("/brands", brandController.getAllBrands);
 
 //CRUD for Jewleries
-app.get("/jeweleries", jeweleryController.getAllJeweleries);
+app.get("/jeweleries", jewelryController.getAllJeweleries);
 app.put("/brands/:id", brandController.updateBrand);
 app.delete("/brands/:id", brandController.deleteBrand);
 app.get("/brands/:brandName", brandController.getBrandByName);
@@ -25,8 +31,8 @@ app.get("/brands", brandController.getAllBrands);
 app.post("/brands", brandController.createBrand);
 
 //CRUD for Jewleries
-app.put("/jeweleries/:id", jeweleryController.updateJewelery);
-app.delete("/jeweleries/:id", jeweleryController.deleteJewelery);
-app.get("/jeweleries/:jeweleryName", jeweleryController.getJeweleryByName);
-app.get("/jeweleries", jeweleryController.getAllJeweleries);
-app.post("/jeweleries", jeweleryController.createJewelery);
+app.put("/jeweleries/:id", jewelryController.updateJewelery);
+app.delete("/jeweleries/:id", jewelryController.deleteJewelery);
+app.get("/jeweleries/:jeweleryName", jewelryController.getJeweleryByName);
+app.get("/jeweleries", jewelryController.getAllJeweleries);
+app.post("/jeweleries", jewelryController.createJewelery);
