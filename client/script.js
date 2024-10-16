f0c711892c664f915ef291106834ff4df20fe895
alinelmuller <alinelmuller@gmail.com>
2024-10-16 06:19:49 -0600

    HTML

--- client/script.js
+++ client/script.js
@@ -1,1 +1,16 @@
-const somethingHere = require(`../models`);
+let carousel = document.querySelector(".carousel");
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let items = document.querySelectorAll(".item"); // Selects all items
+
+(function () {
+  right.addEventListener("click", function () {
+    let itemWidth = items[0].clientWidth; // Assume all items have the same width
+    carousel.scrollLeft += itemWidth;
+  });
+
+  left.addEventListener("click", function () {
+    let itemWidth = items[0].clientWidth; // Assume all items have the same width
+    carousel.scrollLeft -= itemWidth;
+  });
+})();
