e700df539b60bccd76d2b3cc1ead1975346bdbc5
pedrocruz <JayJMarquez05@gmail.com>
2024-10-16 03:01:55 -0500

    Updated HTML | Left comments for what our page will probably look like. Updated JS | Just making the axious call for our search

--- client/script.js
+++ client/script.js
@@ -1,9 +1,13 @@
-// /* ------------{Global Variables}---------------- */
-// const Brand = require(`../models/brand`);
-// const Jewelery = require(`../models/jewelery`);
-// const
-// /* ------------{Functions}---------------- */
-// async function fetchJeweleries(Object) {
-//   const res = await axios.get(`http://localhost:3002/jeweleries`);
-// }
-// /* ------------{Event Listeners}---------------- */
+/* ------------{Global Variables}---------------- */
+const Brand = require(`../models/brand`);
+const Jewelery = require(`../models/jewelery`);
+const button = document.querySelector(`#submit`);
+const priceOfProduct = document.querySelector(`#price-of-product`);
+const productMenuDrop = document.querySelector(`#see-more-button`);
+const productName = document.querySelector(`#product-name`);
+const productDescription = document.querySelector(`#product-description`);
+/* ------------{Functions}---------------- */
+async function userSearchForProduct(name) {
+  const res = await axios.get(`http://localhost:3002/jeweleries/${name}`);
+}
+/* ------------{Event Listeners}---------------- */
